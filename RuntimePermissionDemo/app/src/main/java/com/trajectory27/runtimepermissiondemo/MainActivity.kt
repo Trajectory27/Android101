88fb643a90f31153e4221c3a23697d6e61f68fb9
Trajectory27 <trajectory27@icloud.com>
2023-06-27 21:03:20 +0800

    完善了多请求权限的代码，并且使用了XXPermision框架

--- RuntimePermissionDemo/app/src/main/java/com/trajectory27/runtimepermissiondemo/MainActivity.kt
+++ RuntimePermissionDemo/app/src/main/java/com/trajectory27/runtimepermissiondemo/MainActivity.kt
@@ -9,13 +9,21 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.hjq.permissions.OnPermissionCallback
+import com.hjq.permissions.XXPermissions
 import com.trajectory27.runtimepermissiondemo.databinding.ActivityMainBinding
 import java.lang.Exception
 
+
 /**
- * 拨打电话权限常数
+ * 拨打电话权限常量
  */
 const val CALL_PHONE_PERMISSION = 1
+
+/**
+ * 获取很多权限常量
+ */
+const val A_LOT_PERMISSIONS = 2
 
 /**
  * @author Trajectory27
@@ -33,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         /**
-         * 检查权限 -> 请求权限
+         * 一次请求一个权限 检查权限 -> 请求权限
          */
         binding.btnMakeCall.setOnClickListener {
             if (ContextCompat.checkSelfPermission(
@@ -50,6 +58,74 @@ class MainActivity : AppCompatActivity() {
                 makeCall()
             }
         }
+
+        /**
+         * 一次请求多个权限
+         */
+        val permissions = arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.CAMERA
+        )
+        val unPermissions = ArrayList<String>()
+
+        binding.btnRequestMultiPer.setOnClickListener {
+            unPermissions.clear()
+            for (permission in permissions) {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        permission
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    unPermissions.add(permission)
+                }
+            }
+            if (unPermissions.isNotEmpty()) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    unPermissions.toTypedArray(),
+                    A_LOT_PERMISSIONS
+                )
+            }
+        }
+
+        /**
+         * 使用XXPermission库
+         */
+        binding.btnUseXX.setOnClickListener {
+            XXPermissions.with(this)
+                .permission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                .permission(Manifest.permission.CALL_PHONE)
+                .permission(Manifest.permission.CAMERA)
+                .request(object : OnPermissionCallback {
+                    override fun onGranted(permissions: MutableList<String>, allGranted: Boolean) {
+                        if (!allGranted) {
+                            // 有且仅有部分权限授予成功
+                            Toast.makeText(this@MainActivity, "部分权限授予成功", Toast.LENGTH_SHORT).show()
+                        } else {
+                            // 全部权限授予成功
+                            Toast.makeText(this@MainActivity, "全部权限授予成功", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+
+                    override fun onDenied(
+                        permissions: MutableList<String>,
+                        doNotAskAgain: Boolean
+                    ) {
+                        super.onDenied(permissions, doNotAskAgain)
+                        if (doNotAskAgain) {
+                            // 永久拒绝任意一个权限
+                            Toast.makeText(this@MainActivity, "永久拒绝权限，需要手动开启", Toast.LENGTH_SHORT).show()
+                            XXPermissions.startPermissionActivity(this@MainActivity, permissions)
+                        } else {
+                            // 有权限获取失败，但是所有权限都可以再次直接请求
+                            Toast.makeText(this@MainActivity, "获取权限失败", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+                })
+        }
     }
 
     /**
@@ -62,13 +138,24 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
+
             CALL_PHONE_PERMISSION -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 makeCall()
             } else {
                 Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show()
             }
+
+            A_LOT_PERMISSIONS -> if (grantResults.isNotEmpty()) {
+                for (i in grantResults.indices) {
+                    if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+                }
+            }
         }
     }
+
 
     /**
      * 拨打电话
